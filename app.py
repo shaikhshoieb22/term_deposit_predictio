6ab5e82cac5839b11afb99ad607c24921bfe6224
shaikhshoieb22 <shoiebs@itm.edu>
2021-05-03 01:14:43 +0530

    Final project uploaded

---
+++ app.py
@@ -0,0 +1,36 @@
+import numpy as np
+from flask import Flask, request,  render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    if (output == 0) :
+        prediction_text='Customer will not subscribe'
+    else :
+        prediction_text='Customer will subscribe'
+
+        
+
+
+    return render_template('index.html', prediction_text=prediction_text)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
